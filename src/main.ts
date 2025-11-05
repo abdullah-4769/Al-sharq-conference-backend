7df9baff176100909a778dd702cb52dd0ddfe02f
NasirSultan <rainasirsultan123@gmail.com>
2025-11-05 15:04:05 +0500

    sds

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-  GNU nano 8.3                                           src/main.ts                                                    import { NestFactory } from '@nestjs/core';
+                                                import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
